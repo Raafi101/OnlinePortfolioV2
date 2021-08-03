4c8f97877238978785abb3cf148008c934c90bc5
Raafi Rahman <rafhay101@gmail.com>
2021-08-02 21:47:06 -0400

    Mobile vid fix

--- src/Pages/Components/ProjectDivs/Coding/DigitClassifier.js
+++ src/Pages/Components/ProjectDivs/Coding/DigitClassifier.js
@@ -25,7 +25,7 @@ function DigitClassifier() {
                         how to save a model for future use.
                     </h2>
                     <div id='vidDiv'>
-                        <video id='ProjectOddCSVid' controls>
+                        <video id='ProjectOddCSVid' width='300px' controls>
                             <source src='https://portfoliostorage.s3.us-east-2.amazonaws.com/Images/DigitVid.mp4#t=0.1' type='video/mp4'/>
                         </video>
                     </div>

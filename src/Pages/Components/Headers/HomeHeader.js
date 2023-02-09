f4dfe7d12ffcf55ab4b49e5d03f31b6311617340
raafi101 <rafhay101@gmail.com>
2023-02-08 21:28:38 -0500

    Update HomeHeader.js

--- src/Pages/Components/Headers/HomeHeader.js
+++ src/Pages/Components/Headers/HomeHeader.js
@@ -12,7 +12,7 @@ function HomeHeader() {
         <div id='animation'>
             <div id='header'>
                 <h1 id='name' data-aos='fade-right' data-aos-duration='500'>Raafi Rahman</h1>
-                <h1 id='title' data-aos='fade-left' data-aos-delay='100' data-aos-duration='500'>ML Engineer, Data Scientist, SW Engineer</h1>
+                <h1 id='title' data-aos='fade-left' data-aos-delay='100' data-aos-duration='500'>ML/AI/SW Engineer, AI Researcher, Data Scientist</h1>
                 <h1 id='quote' data-aos='fade-right' data-aos-delay='200' data-aos-duration='500'>“Mathematics is the language in which God has written the universe”</h1>        
                 <h1 id='author' data-aos='fade-left' data-aos-delay='300' data-aos-duration='500'>― Galileo Galilei</h1>
             </div>
